ed7b3871ff26be310d1e999acc22a49c0503bc16
bacng <nhb.fit.hut@gmail.com>
2022-09-29 23:00:59 +0700

    add articels

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_29_154657) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_29_155651) do
+  create_table "articles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tours", force: :cascade do |t|
     t.string "title"
     t.string "description"
